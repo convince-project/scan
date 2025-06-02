f4b445304a8a5ed2b9b4c76d650432078b46acca
Enrico Ghiorzi <enrico.ghiorzi@edu.unige.it>
2025-06-02 13:36:35 +0200

    Make manifest more informative and add human_panic crate
    Signed-off-by: Enrico Ghiorzi <enrico.ghiorzi@edu.unige.it>

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ use clap::Parser;
 use scan::Cli;
 
 fn main() -> anyhow::Result<()> {
+    human_panic::setup_panic!();
     let cli = Cli::parse();
     env_logger::Builder::new()
         .filter_level(cli.verbosity.log_level_filter())
