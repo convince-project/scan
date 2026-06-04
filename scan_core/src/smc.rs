0cc2d6bce676022b8881257e1d60733b2133b69e
Enrico Ghiorzi <enrico.ghiorzi@edu.unige.it>
2026-06-04 13:46:45 +0200

    Use more precise float operations
    Signed-off-by: Enrico Ghiorzi <enrico.ghiorzi@edu.unige.it>

--- scan_core/src/smc.rs
+++ scan_core/src/smc.rs
@@ -5,13 +5,16 @@
 
 /// Computes Okamoto bound for given confidence and precision.
 pub fn okamoto_bound(confidence: f64, precision: f64) -> f64 {
-    (2f64 / (1f64 - confidence)).ln() / (2f64 * precision.powf(2f64))
+    (2f64 / (1f64 - confidence)).ln() * 0.5f64 / precision.powi(2)
 }
 
 /// Computes adaptive bound for given confidence, precision and (partial) experimental results.
 pub fn adaptive_bound(avg: f64, confidence: f64, precision: f64) -> f64 {
-    4f64 * okamoto_bound(confidence, precision)
-        * (0.25f64 - ((avg - 0.5f64).abs() - (2f64 * precision / 3f64)).powf(2f64))
+    okamoto_bound(confidence, precision)
+        * precision
+            .mul_add(-2f64 / 3f64, (avg - 0.5f64).abs())
+            .powi(2)
+            .mul_add(-4f64, 1f64)
 }
 
 /// Computes precision for given experimental results and confidence
@@ -21,9 +24,9 @@ pub fn derive_precision(s: u32, f: u32, confidence: f64) -> f64 {
     let avg = s as f64 / n as f64;
     let k = 2f64 * (2f64 / (1f64 - confidence)).ln();
     // Compute quadratic equation coefficients.
-    let a = (n as f64) + (4f64 * k / 9f64);
-    let b = -4f64 * k * (avg - 0.5f64).abs() / 3f64;
-    let c = k * ((avg - 0.5f64).powf(2f64) - 0.25f64);
+    let a = k.mul_add(4f64 / 9f64, n as f64);
+    let b = -(4f64 / 3f64) * k * (avg - 0.5f64).abs();
+    let c = k * (avg.powi(2) - avg);
     // Take (larger positive) quadratic equation solution.
-    (-b + (b.powf(2f64) - 4f64 * a * c).sqrt()) / (2f64 * a)
+    (a.mul_add(-4f64 * c, b.powi(2)).sqrt() - b) / (2f64 * a)
 }
