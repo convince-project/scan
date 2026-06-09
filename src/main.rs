5cf75bdbe267b9663f92e69e0b14cf43a65b7565
Enrico Ghiorzi <enrico.ghiorzi@edu.unige.it>
2026-06-09 14:49:11 +0200

    Remove human-panic dependency
    SCAN is not ready for this and it often panics when it should return an error.
    When all panics are handled appropriately, human-panic can be used again.
    
    Signed-off-by: Enrico Ghiorzi <enrico.ghiorzi@edu.unige.it>

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,6 @@ use clap::Parser;
 use scan::Cli;
 
 fn main() -> anyhow::Result<()> {
-    human_panic::setup_panic!();
     let cli = Cli::parse();
     env_logger::Builder::new()
         .filter_level(cli.verbosity.log_level_filter())
